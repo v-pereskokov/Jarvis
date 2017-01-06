498aa42f9c6afb78a86d806ced809c6c23fbb86a
vladpereskokov <vladpereskokov@mail.ru>
2017-01-06 12:18:54 +0300

    travis tests

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build Status](https://travis-ci.org/vladpereskokov/Jarvis.svg?branch=jarvis)](https://travis-ci.org/vladpereskokov/Jarvis)
--- tests/test.cpp
+++ tests/test.cpp
@@ -3,8 +3,5 @@
 #include "../include/Jarvis.hpp"
 
 TEST_CASE("Jarvis parse voice", "[voice]" ) {
-  REQUIRE(Jarvis::Jarvis::getStringVoice() == "включить");
-  REQUIRE(Jarvis::Jarvis::getStringVoice() == "Джарвис");
-  REQUIRE(Jarvis::Jarvis::getStringVoice() == "бежать");
   REQUIRE(Jarvis::Jarvis::getStringVoice() == "дубина");
 }
