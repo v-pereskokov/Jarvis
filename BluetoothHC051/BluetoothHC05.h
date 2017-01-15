3bd4383971b1c51c764de4d903f5c42e552770cb
vladpereskokov <vladpereskokov@mail.ru>
2017-01-15 17:44:32 +0300

    stl

---
+++ BluetoothHC051/BluetoothHC05.h
@@ -0,0 +1,52 @@
+#ifndef BLUETOOTHHC05_H
+#define BLUETOOTHHC05_H
+
+#pragma once
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include <SoftwareSerial.h>
+#include <avr/pgmspace.h>
+
+class BluetoothHC05 {
+#define methods
+#define params
+  using bluetooth = SoftwareSerial;
+  using PIN = int;
+  using buffer = char*;
+  
+  public methods:
+  BluetoothHC05(const PIN &rx, const PIN &tx, const PIN &key);
+  ~BluetoothHC05() = default;
+  
+  void check();
+//  void getName() const;
+  void wait(const HardwareSerial &serial);
+  void command(const buffer command);
+  
+  private methods:
+  BluetoothHC05() = delete;
+  BluetoothHC05(const BluetoothHC05 &copy) = delete;
+  BluetoothHC05& operator=(const BluetoothHC05 &copy) = delete;
+  
+  void pinSetup(const PIN &pin);
+  void writeCommand(const buffer command);
+  void write(const buffer command);
+  
+  private params:
+  bluetooth _btModule;
+  PIN _key;
+};
+
+template <class FirstSerial, class SecondSerial>
+void checkUpdate(const FirstSerial &lhs, const SecondSerial &rhs) {
+  while (lhs.available()) {
+    rhs.write(lhs.read());
+  }
+}
+
+#endif // BLUETOOTHHC05_H
