ea32ef3e3822116ebdc19b7dc4327665c5a1af4e
vladpereskokov <vladpereskokov@mail.ru>
2017-01-18 22:24:08 +0300

    check on/off

--- GUI/GUI/smartdevice.cpp
+++ GUI/GUI/smartdevice.cpp
@@ -2,187 +2,191 @@
 
 SmartDevice::SmartDevice(QWidget *parent, const QString deviceBluetooth) : QPushButton{parent}
 {
-    deviceBluetoothName = deviceBluetooth;
-    groupName = "defaultGroup";
-    deviceName = "defaultName";
-    deviceID = 0;
-    deviceStatus = false;
-    factory = new Jarvis::Devices::DeviceFactory;
+  deviceBluetoothName = deviceBluetooth;
+  groupName = "defaultGroup";
+  deviceName = "defaultName";
+  deviceID = 0;
+  deviceStatus = false;
+  factory = new Jarvis::Devices::DeviceFactory;
 }
 
 SmartDevice::SmartDevice(QWidget *parent, const QString& deviceBluetooth,
                          const QString& groupName, const QString& deviceName, int id) : SmartDevice{parent, deviceBluetooth}
 {
-
-
-    if(groupName.isEmpty())
-        QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Empty group name"});
-    else
-        this->groupName = groupName;
-
+  
+  
+  if(groupName.isEmpty())
+    QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Empty group name"});
+  else
+    this->groupName = groupName;
+    
     if(deviceName.isEmpty())
-        QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Empty device name"});
+      QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Empty device name"});
     else
-        this->deviceName = deviceName;
-
-    if(id < 0)
+      this->deviceName = deviceName;
+      
+      if(id < 0)
         QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Invalid device ID"});
-    else
+      else
         deviceID = id;
-}
+        }
 
 SmartDevice::~SmartDevice()
 {
-    delete factory;
+  delete factory;
 }
 
 
 void SmartDevice::startFabric(std::string devicePort)
 {
-    device = (*factory)(deviceBluetoothName.toUtf8().constData(), deviceName.toUtf8().constData(), devicePort);
+  device = (*factory)(deviceBluetoothName.toUtf8().constData(), deviceName.toUtf8().constData(), devicePort);
 }
 
 bool SmartDevice::setGroupName(const QString& name)
 {
-    QString temp = groupName;
-
-    if(name.isEmpty())
+  QString temp = groupName;
+  
+  if(name.isEmpty())
     {
-        QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Empty group name"});
-        return false;
+    QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Empty group name"});
+    return false;
     }
+  else
+    {
+    groupName = name;
+    if(uploadDeviceConfig())
+      return true;
     else
-    {
-        groupName = name;
-        if(uploadDeviceConfig())
-            return true;
-        else
-        {
-            groupName = temp;
-            QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Cannot upload config"});
-            return false;
-        }
-
+      {
+      groupName = temp;
+      QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Cannot upload config"});
+      return false;
+      }
+    
     }
-    return true;
+  return true;
 }
 
 bool SmartDevice::setDeviceName(const QString &name)
 {
-    QString temp = deviceName;
-
-    if(name.isEmpty())
+  QString temp = deviceName;
+  
+  if(name.isEmpty())
     {
-        QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Empty device name"});
-        return false;
+    QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Empty device name"});
+    return false;
     }
+  else
+    {
+    deviceName = name;
+    if(uploadDeviceConfig())
+      return true;
     else
-    {
-        deviceName = name;
-        if(uploadDeviceConfig())
-            return true;
-        else
-        {
-            deviceName = temp;
-            QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Cannot upload config"});
-            return false;
-        }
+      {
+      deviceName = temp;
+      QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Cannot upload config"});
+      return false;
+      }
     }
-    return true;
+  return true;
 }
 
 
 
 QString SmartDevice::getDeviceBluetoothName() const
 {
-    return deviceBluetoothName;
+  return deviceBluetoothName;
 }
 bool  SmartDevice::setDeviceID(int id)
 {
-    int temp = deviceID;
-    if(id < 0)
+  int temp = deviceID;
+  if(id < 0)
     {
-        QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Invalid device ID"});
-        return false;
+    QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Invalid device ID"});
+    return false;
     }
+  else
+    {
+    deviceID = id;
+    if(uploadDeviceConfig())
+      return true;
     else
-    {
-        deviceID = id;
-        if(uploadDeviceConfig())
-            return true;
-        else
-        {
-            deviceID = temp;
-            QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Cannot upload config"});
-            return false;
-        }
+      {
+      deviceID = temp;
+      QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Cannot upload config"});
+      return false;
+      }
     }
-    return true;
+  return true;
 }
 
 bool SmartDevice::turnOnDevice()
 {
-    bool temp =  deviceStatus;
-    deviceStatus = true;
-
-    device->on();
-
-    if(uploadDeviceConfig())
-        return true;
-    else
+  bool temp =  deviceStatus;
+  deviceStatus = true;
+  
+  device->connect();
+  device->on();
+  device->disconnect();
+  
+  if(uploadDeviceConfig())
+    return true;
+  else
     {
-        deviceStatus = temp;
-        QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Cannot upload config"});
-        return false;
+    deviceStatus = temp;
+    QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Cannot upload config"});
+    return false;
     }
 }
 
 bool SmartDevice::turnOffDevice()
 {
-    bool temp =  deviceStatus;
-    deviceStatus = false;
-
-    device->off();
-
-    if(uploadDeviceConfig())
-        return true;
-    else
+  bool temp =  deviceStatus;
+  deviceStatus = false;
+  
+  device->connect();
+  device->off();
+  device->disconnect();
+  
+  if(uploadDeviceConfig())
+    return true;
+  else
     {
-        deviceStatus = temp;
-        QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Cannot upload config"});
-        return false;
+    deviceStatus = temp;
+    QMessageBox::information(nullptr, QString{"warning"}, QString{"Error. Cannot upload config"});
+    return false;
     }
 }
 
 bool SmartDevice::getDeviceStatus() const
 {
-    return deviceStatus;
+  return deviceStatus;
 }
 
 
 QString SmartDevice::getGroupName() const
 {
-    return groupName;
+  return groupName;
 }
 
 QString SmartDevice::getDeviceName() const
 {
-    return deviceName;
+  return deviceName;
 }
 
 int SmartDevice::getDeviceID() const
 {
-    return deviceID;
+  return deviceID;
 }
 
 bool SmartDevice::updateDeviceConfig()
 {
-    return true;
+  return true;
 }
 
 bool SmartDevice::uploadDeviceConfig()
 {
-    return true;
+  return true;
 }
 
 
