6703dbe7755745305a9b2a979afba1af82d1dbff
vladpereskokov <vladpereskokov@mail.ru>
2017-01-18 18:50:28 +0300

    jarvishome make

---
+++ JarvisHome/main/main.cpp
@@ -0,0 +1,5 @@
+#include "../src/run.cpp"
+
+int main() {
+  return run();
+}
---
+++ JarvisHome/src/run.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <string>
+#include <vector>
+#include <queue>
+#include "../../Jarvis/include/Jarvis.hpp"
+
+#define SUCCESS 0
+
+int run() {
+  std::cout << "hello\n";
+  return SUCCESS;
+}
+
+
+
